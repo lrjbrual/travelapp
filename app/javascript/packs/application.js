6f1979d15072feca3540f28a2ab52952d9743db3
Ryan Jeff Brual <lrjbrual@live.com>
2019-07-11 13:16:44 +0200

    update the themes needed

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,28 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+require("jquery")
+
+import './bootstrap_custom.js'
+import flatpickr from "flatpickr"
+require("flatpickr/dist/themes/material_green.css");
+
+document.addEventListener("turbolinks:load", () => {
+  flatpickr("[data-behavior='flatpickr']", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "d-m-Y",
+  })
+})
